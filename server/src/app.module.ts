78c6f57b29eb811ea466660f6f298e5c4bf56516
Anton <toxajlb@yandex.ru>
2023-03-23 16:29:34 +0300

    created dto, auth service with jwt tokens

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { getJwtConfig } from './config/jwt.config';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, JwtStrategy, PrismaService],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
+  ],
 })
 export class AppModule {}
