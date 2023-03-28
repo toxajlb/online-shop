d638faf1535502876326732b8babf1f65782049b
Anton <toxajlb@yandex.ru>
2023-03-28 16:30:39 +0300

    Created reviews

--- server/src/review/review.controller.ts
+++ server/src/review/review.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { ReviewDto } from './review.dto';
 import { ReviewService } from './review.service';
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @UsePipes(new ValidationPipe())
+  @Get()
+  async getAll() {
+    return this.reviewService.getAll();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post('leave/:productId')
+  async leaveReview(
+    @CurrentUser('id') id: number,
+    @Body() dto: ReviewDto,
+    @Param('productId') productId: string,
+  ) {
+    return this.reviewService.create(id, dto, +productId);
+  }
 }
