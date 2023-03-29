bfa1f809c691c86aa6e81dada9c776ec517718ce
Anton <toxajlb@yandex.ru>
2023-03-29 12:36:25 +0300

    Created pagination part

--- server/src/pagination/pagination.module.ts
+++ server/src/pagination/pagination.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PaginationController } from './pagination.controller';
 import { PaginationService } from './pagination.service';
 
 @Module({
-  controllers: [PaginationController],
   providers: [PaginationService],
+  exports: [PaginationService],
 })
 export class PaginationModule {}
