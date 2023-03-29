f9e49d077b5f58ac092ae29675600ffa0b794acd
Anton <toxajlb@yandex.ru>
2023-03-29 12:26:12 +0300

    created statistic part

--- server/src/statistic/statistic.controller.ts
+++ server/src/statistic/statistic.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
 import { StatisticService } from './statistic.service';
 
 @Controller('statistic')
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
+
+  @Get('main')
+  @Auth()
+  getMainStatistics(@CurrentUser('id') id: number) {
+    return this.statisticService.getMain(id);
+  }
 }
--- server/src/statistic/statistic.module.ts
+++ server/src/statistic/statistic.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { StatisticService } from './statistic.service';
-import { StatisticController } from './statistic.controller';
 import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { StatisticController } from './statistic.controller';
+import { StatisticService } from './statistic.service';
 
 @Module({
   controllers: [StatisticController],
-  providers: [StatisticService, PrismaService],
+  providers: [StatisticService, PrismaService, UserService],
 })
 export class StatisticModule {}
