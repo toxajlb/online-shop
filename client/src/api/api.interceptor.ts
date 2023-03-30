15b2fc802dbd4bdc03d9187cef3a0983c0dee36f
Anton <toxajlb@yandex.ru>
2023-03-30 17:16:12 +0300

    modified interceptor

--- client/src/api/api.interceptor.ts
+++ client/src/api/api.interceptor.ts
@@ -1,8 +1,44 @@
 import axios from "axios";
 
-import { getContentType } from "./api.helper";
+import { getAccessToken } from "@/services/auth/auth.helper";
+import { errorCatch, getContentType } from "./api.helper";
 
 const instance = axios.create({
   baseURL: process.env.SERVER_URL,
   headers: getContentType(),
 });
+
+instance.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+
+  if (config && config.headers && accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+instance.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      (error.response.status === 401 ||
+        errorCatch(error) === "jwt expired" ||
+        errorCatch(error) === "jwt must be provided") &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        return instance.request(originalRequest)
+      }
+      catch (error) {
+        if (errorCatch(error) === 'jwt expired')
+        //delete tokens
+      }
+    }
+    throw error
+  }
+);
--- client/src/services/auth/auth.helper.ts
+++ client/src/services/auth/auth.helper.ts
@@ -2,6 +2,15 @@ import Cookies from "js-cookie";
 
 import { IAuthResponse, ITokens } from "@/store/user/user.interface";
 
+export const getAccessToken = () => {
+  const accessToken = Cookies.get("accessToken");
+  return accessToken || null;
+};
+
+export const getUserFromStorage = () => {
+  return JSON.parse(localStorage.getItem("user") || "{}");
+};
+
 export const saveTokensStorage = (data: ITokens) => {
   Cookies.set("accessToken", data.accessToken);
   Cookies.set("refreshToken", data.refreshToken);
