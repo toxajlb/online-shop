5674c3153d36e4b627f4bef66d3f7a8fdaf95466
Anton <toxajlb@yandex.ru>
2023-03-31 14:08:45 +0300

    created review-service

--- client/src/services/category.service.ts
+++ client/src/services/category.service.ts
@@ -2,40 +2,40 @@ import { ICategory } from "@/types/category.interface";
 
 import { instance } from "@/api/api.interceptor";
 
-const CATEGORY = "category";
+const CATEGORIES = "categories";
 
 export const CategoryService = {
   async getAll() {
     return instance<ICategory[]>({
-      url: CATEGORY,
+      url: CATEGORIES,
       method: "GET",
     });
   },
 
   async getById(id: string | number) {
     return instance<ICategory>({
-      url: `${CATEGORY}/${id}`,
+      url: `${CATEGORIES}/${id}`,
       method: "GET",
     });
   },
 
   async getBySlug(slug: string) {
     return instance<ICategory>({
-      url: `${CATEGORY}/by-slug/${slug}`,
+      url: `${CATEGORIES}/by-slug/${slug}`,
       method: "GET",
     });
   },
 
   async create() {
     return instance<ICategory>({
-      url: CATEGORY,
+      url: CATEGORIES,
       method: "POST",
     });
   },
 
   async update(id: string | number, name: string) {
     return instance<ICategory>({
-      url: `${CATEGORY}/${id}`,
+      url: `${CATEGORIES}/${id}`,
       method: "PUT",
       data: { name },
     });
@@ -43,7 +43,7 @@ export const CategoryService = {
 
   async delete(id: string | number) {
     return instance<ICategory>({
-      url: `${CATEGORY}/${id}`,
+      url: `${CATEGORIES}/${id}`,
       method: "DELETE",
     });
   },
