30777c96b5f7562f585db0e4c0738f07f90efef9
Anton <toxajlb@yandex.ru>
2023-04-01 15:32:34 +0300

    created AuthProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import AuthProvider from "@/providers/auth-provider/AuthProvider";
+import { TypeComponentAuthFields } from "@/providers/auth-provider/auth-page.types";
+
 import "@/assets/styles/globals.css";
 
 import { persistor, store } from "@/store/store";
@@ -16,12 +19,17 @@ const queryClient = new QueryClient({
   },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & TypeComponentAuthFields) {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
+          <AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+            <Component {...pageProps} />
+          </AuthProvider>
         </PersistGate>
       </Provider>
     </QueryClientProvider>
--- client/src/providers/auth-provider/AuthProvider.tsx
+++ client/src/providers/auth-provider/AuthProvider.tsx
@@ -1,7 +1,39 @@
-import { FC, PropsWithChildren } from "react";
+import Cookies from "js-cookie";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { FC, PropsWithChildren, useEffect } from "react";
 
-const AuthProvider: FC<PropsWithChildren<unknown>> = () => {
-  return <div>AuthProvider</div>;
+import { useActions } from "@/hooks/useActions";
+import { useAuth } from "@/hooks/useAuth";
+
+import { TypeComponentAuthFields } from "./auth-page.types";
+import { getAccessToken } from "@/services/auth/auth.helper";
+
+const DynamicCheckRole = dynamic(() => import("./CheckRole"), { ssr: false });
+
+const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = ({
+  Component: { isOnlyUser },
+  children,
+}) => {
+  const { user } = useAuth();
+  const { checkAuth, logout } = useActions();
+  const pathname = useRouter();
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    if (accessToken) checkAuth();
+  }, []);
+
+  useEffect(() => {
+    const refreshToken = Cookies.get("refreshToken");
+    if (!refreshToken && user) logout();
+  }, [pathname]);
+
+  return isOnlyUser ? (
+    <DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
+  ) : (
+    <>{children}</>
+  );
 };
 
 export default AuthProvider;
