05a87bb3bb24a271f5788da4377efa08a6877059
Anton <toxajlb@yandex.ru>
2023-04-02 10:40:57 +0300

    Created authorization part: Auth, validation, Loader, Field

--- client/src/components/screens/auth/Auth.tsx
+++ client/src/components/screens/auth/Auth.tsx
@@ -1,14 +1,97 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
 
 import Heading from "@/ui/Heading";
+import Loader from "@/ui/Loader";
 import Meta from "@/ui/Meta";
 import Button from "@/ui/button/Button";
+import Field from "@/ui/input/Field";
+
+import { IEmailPassword } from "@/store/user/user.interface";
+
+import { useActions } from "@/hooks/useActions";
+import { useAuth } from "@/hooks/useAuth";
+
+import { useAuthRedirect } from "./useAuthRedirect";
+import { validEmail } from "./valid-email";
 
 const Auth: FC = () => {
+  useAuthRedirect();
+
+  const { isLoading } = useAuth();
+  const { login, register } = useActions();
+
+  const [type, setType] = useState<"login" | "register">("login");
+
+  const {
+    register: formRegister,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IEmailPassword>({ mode: "onChange" });
+
+  const onSubmit: SubmitHandler<IEmailPassword> = (data) => {
+    if (type === "login") login(data);
+    else register(data);
+
+    reset();
+  };
+
   return (
     <Meta title="Auth">
-      <Heading>Auth</Heading>
-      <Button variant="orange">Auth</Button>
+      <section className="flex h-screen">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="rounded-lg bg-white shadow-sm p-8 m-auto"
+        >
+          <Heading className="capitalize text-center mb-4">{type}</Heading>
+
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <Field
+                {...formRegister("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: validEmail,
+                    message: "Please enter a valid email address",
+                  },
+                })}
+                placeholder="Email"
+                error={errors.email?.message}
+              />
+              <Field
+                {...formRegister("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Min length should more 6 symbols",
+                  },
+                })}
+                type="password"
+                placeholder="Password"
+                error={errors.password?.message}
+              />
+              <Button type="submit" variant="orange">
+                Submit
+              </Button>{" "}
+              <div>
+                <button
+                  type="button"
+                  className="inline-block opacity-20 mt-3 text-sm"
+                  onClick={() =>
+                    setType(type === "login" ? "register" : "login")
+                  }
+                >
+                  {type === "login" ? "Register" : "Login"}
+                </button>
+              </div>
+            </>
+          )}
+        </form>
+      </section>
     </Meta>
   );
 };
