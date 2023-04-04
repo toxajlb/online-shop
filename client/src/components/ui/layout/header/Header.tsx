5e349f878dc98679306c9e352d89590ac320c8c4
Anton <toxajlb@yandex.ru>
2023-04-04 11:50:37 +0300

    Created Favorites page

--- client/src/components/ui/layout/header/Header.tsx
+++ client/src/components/ui/layout/header/Header.tsx
@@ -1,7 +1,23 @@
-import { FC, PropsWithChildren } from "react";
+import Link from "next/link";
+import { FC } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
 
-const Header: FC<PropsWithChildren<unknown>> = ({ children }) => {
-  return <div className="bg-secondary w-full">Header</div>;
+const Header: FC = () => {
+  return (
+    <header
+      className="bg-secondary w-full py-6 px-6 grid"
+      style={{ gridTemplateColumns: "1fr 3fr 1.2fr" }}
+    >
+      <Link className="text-white" href="/">
+        Next shop
+      </Link>
+      <div className="flex items-center justify-end gap-10">
+        <Link href="/favorites" className="text-white">
+          <AiOutlineHeart size={28} />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
